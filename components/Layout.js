ac4c6ec061a195f2ab144f79c35e8ba199b3efd9
niazisaad2198 <niazisaad2198@gmail.com>
2022-07-02 21:19:57 +0500

    Used Navbar component in Layout.

--- components/Layout.js
+++ components/Layout.js
@@ -1,15 +1,16 @@
-import React from "react";
 import Head from "next/head";
 import { Box } from "@chakra-ui/react";
+import Navbar from "./Navbar";
 
-const Layout = () => {
+
+const Layout = ({children}) => {
   return (
     <>
       <Head>
         <title>Real Estate</title>
       </Head>
       <Box maxWidth="1280px" m="auto">
-        <header>Navbar</header>
+        <header><Navbar/></header>
         <main>{children}</main>
         <footer>Footer</footer>
       </Box>
