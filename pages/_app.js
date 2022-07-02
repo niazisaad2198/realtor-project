4126759fe363ab116f71ae631ee0f1bc4250b525
niazisaad2198 <niazisaad2198@gmail.com>
2022-07-02 21:21:38 +0500

    Wrapped Layout around "root" component so that new Layout works.

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,19 @@
-import "../styles/globals.css";
-
+import Router from "next/router";
+import Head from "next/head";
+import NProgress from "nprogress";
 import { ChakraProvider } from "@chakra-ui/react";
+import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Head></Head>
+      <ChakraProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </>
   );
 }
 
